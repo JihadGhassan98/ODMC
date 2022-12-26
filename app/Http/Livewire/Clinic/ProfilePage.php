f3c7445e4698c85c3b56dd46ccb885fb0e9887c7
JihadGhassan98 <64194474+JihadGhassan98@users.noreply.github.com>
2022-12-26 07:56:28 +0300

    started fe pages

---
+++ app/Http/Livewire/Clinic/ProfilePage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Clinic;
+
+use Livewire\Component;
+
+class ProfilePage extends Component
+{
+    public function render()
+    {
+        return view('livewire.clinic.profile-page');
+    }
+}
